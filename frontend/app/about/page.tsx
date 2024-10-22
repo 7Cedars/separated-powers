bcee6d9f193396dd65a3f15ae3a005c6c5d736f3
Seven Cedars <Cedars7@proton.me>
2024-10-23 00:26:27 +0100

    small bug fixes

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import React, { useState } from "react";
-import { lawContracts } from "@/context/lawContracts";
-import { useWriteContract } from "wagmi";
-import { agCoinsAbi } from "@/context/abi";
 import Link from "next/link";
 
 export default function Page() { 
     return (
       <section className="w-full flex flex-col justify-center items-center m-4 text-center mt-20">
           <div className="max-w-3xl bg-gray-200 text-center border border-gray-200 px-4 rounded-lg shadow-lg z-10 mb-6">
-                    <h2 className="font-bold"> Goal: Fund projects that are ‘aligned’ with core values of the agDAO. </h2>        
-                    <div 
-                        className="h-fit mt-0" >
+                    <h2 className="text-lg font-bold mx-4"> What is this dashboard about? </h2>
+                    <p className="mb-2"> AgDAO is a very simple example implementation of the Separated Powers Protocol.</p>
+                    <p> It showcases different rules that can be created to assign accounts to roles, and how checks and balances between roles can be implemented.</p>
+                    <div className="h-fit mt-0" >
+
+                        <h2 className="text-lg font-bold mt-4"> What is AgDao? </h2>
+                        <h2 className="font-semibold">
+                            Aligned Grants DAO aims to fund projects that are ‘aligned’ with the core values of the agDAO. </h2>
+                        <h2 className="font-semibold mt-2"> It functions along the following mechanisms: </h2>
                         <ul 
                             className="list-decimal list-inside"
                             > 
@@ -24,7 +27,7 @@ export default function Page() {
                             <li>Whales can propose new laws, senior can accept them, and the admin implements them.</li>
                         </ul>
 
-                        <h2 className="font-bold mt-4"> See below for the concrete implementation of AgDAO </h2>
+                        <h2 className="font-bold my-4"> See the actions tab in the dashboard for concrete implementation of protocol rules. </h2>
                     </div>
                 </div>
 
