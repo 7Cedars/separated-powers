8815c29caf7098947b0f65eaae8d3c858de8082f
Seven Cedars <Cedars7@proton.me>
2025-01-17 13:52:23 +0000

    bug: fix ui scaling issues landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,31 +18,36 @@ import Image from 'next/image'
 const useCases = [
     {
         value: "Increase efficiency by delegating tasks",
-        image: "/lawList.png", 
+        image: "/lawList.png",
+        imageNarrow: "/lawListNarrow.png", 
         detail: "Define tasks in external programmable contracts, called laws, and assign them to specific roles.",
         examples: ["example 1", "example 2", "example 3"]
     }, 
     {
         value: "Use oracles to automate tasks seamlessly",
         image: "/proposal.png", 
+        imageNarrow: "/proposalNarrow.png", 
         detail: "Seamlessly integrate with time based oracles to execute laws automatically.",
         examples: ["example 1", "example 2", "example 3"]
     },
     {
         value: "Increase security through checks and balances",
         image: "/law.png", 
+        imageNarrow: "/lawListNarrow.png", 
         detail: "Only allow tasks to be executed if certain conditions have been met. For example, if other roles have (not) passed like-for-like proposals.",
         examples: ["example 1", "example 2", "example 3"]
     }, 
     {
         value: "Distribute power by designating roles to diverse groups",
         image: "/roles.png", 
+        imageNarrow: "/rolesNarrow.png", 
         detail: "Create roles for builders, community leaders, token holders, and more.",
         examples: ["example 1", "example 2", "example 3"]
     },
     {
         value: "Cut the noise, while remaining transparent",
         image: "/home.png", 
+        imageNarrow: "/homeNarrow.png", 
         detail: "Each roles submits proposals, votes and executes within their role defined group, but open for all to see.",
         examples: ["example 1", "example 2", "example 3"]
     },
@@ -91,13 +96,23 @@ export default function Page() {
                     <div className="min-w-[20vw] h-full flex flex-col justify-center items-center snap-center snap-always gap-2" />
                     {
                         useCases.map((useCase, index) => (
-                            <div className="min-w-[60vw] h-full flex flex-col justify-center items-center text-slate-50 snap-center snap-always gap-2" key={index}> 
+                            <div className="min-w-[60vw] h-full flex flex-col justify-center items-center text-slate-50 snap-center snap-always" key={index}> 
                                 <div className="w-full h-fit text-center text-pretty text-lg sm:text-2xl text-slate-200 py-4">
                                     {useCase.value}
                                 </div> 
-                                <div className="grow w-full max-h-[60vw]" style = {{position: 'relative', width: '100%', height: '100%'}}>
+                                <div className="shrink w-0 max-h-0 md:grow md:w-full md:max-h-[60vw]" style = {{position: 'relative', width: '100%', height: '100%'}}>
                                     <Image 
                                         src={useCase.image} 
+                                        className = "rounded-md" 
+                                        style={{objectFit: "contain", objectPosition: "center"}}
+                                        fill={true}
+                                        alt="Screenshot Separated Powers"
+                                        >
+                                    </Image>
+                                 </div>
+                                 <div className="md:shrink md:w-0 md:max-h-0 grow w-full max-h-[60vw]" style = {{position: 'relative', width: '100%', height: '100%'}}>
+                                    <Image 
+                                        src={useCase.imageNarrow} 
                                         className = "rounded-md" 
                                         style={{objectFit: "contain", objectPosition: "center"}}
                                         fill={true}
